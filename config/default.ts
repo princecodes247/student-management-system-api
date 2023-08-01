14544faad0f3e2cdfc8cfe92c561fee0ff371f12
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2023-08-01 22:32:19 +0100

    add database

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,7 @@
 export default {
   port: process.env.PORT || 3122,
-  dbUri: "mongodb://127.0.0.1:27017/nyx-primetrust",
+  // dbUri: "mongodb://127.0.0.1:27017/nyx-primetrust",
+  dbUri: "mongodb+srv://Oracle247:oracle247@cluster0.qvwi6.mongodb.net/sms?retryWrites=true&w=majority",
   saltWorkFactor: 10,
   accessTokenTtl: "15m",
   refreshTokenTtl: "1y",
